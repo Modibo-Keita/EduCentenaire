f36d419132670204ce8a7d9c524a772bb538448f
Modibo Keita <Keitavan.10@outlook.fr>
2025-12-03 13:16:14 +0000

    première version fonctionnelle du site et preparation du deploiement sur netlify

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Impossible de trouver l’élément #root dans index.html");
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
